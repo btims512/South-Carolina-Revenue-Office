ff1687a9e53ce462806cea372b1bbe397aeabab1
Ben <71402472+btims512@users.noreply.github.com>
2023-03-11 20:48:17 -0700

    sub menu started

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import NavBar from './Components/NavBar'
+import NavBar from "./Components/NavBar";
 
-import './App.css';
-import "./index.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'mdbreact/dist/css/mdb.css';
-
+import "./App.css";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function App() {
   return (
     <div className="App">
-    <NavBar />
+      <NavBar />
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,43 @@
 import React, { useState } from 'react';
 import { MDBContainer, MDBCollapse, MDBNavbar, MDBNavbarToggler, MDBBtn } from 'mdb-react-ui-kit';
+import logo from '../images/logo.png';
+
+import { HiMagnifyingGlass } from "react-icons/hi2";
+
+
 
 const MenuIcon = ({ isOpen }) => (
-  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '1.5rem' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '1.5rem', boxShadow: 'none' }}>
     <div
-      style={{ width: '1.5rem', height: '0.2rem', backgroundColor: '#fff', borderRadius: '10px', transform: isOpen ? 'rotate(45deg) translate(7px, 7px)' : 'none' }}
+      style={{
+        width: '2rem',
+        height: '0.2rem',
+        backgroundColor: '#fff',
+        transform: isOpen ? 'rotate(45deg) translate(5px, 6px)' : 'none',
+        transition: 'transform 0.2s ease-in-out'
+      }}
     />
     <div
-      style={{ width: '1.5rem', height: '0.2rem', backgroundColor: '#fff', borderRadius: '10px', transform: isOpen ? 'rotate(-45deg) translate(8px, -7px)' : 'none' }}
+      style={{
+        width: '2rem',
+        height: '0.2rem',
+        backgroundColor: '#fff',
+        opacity: isOpen ? 0 : 1,
+        transition: 'opacity 0.2s ease-in-out',
+        transform: isOpen ? 'none' : 'translateX(0)',
+      }}
     />
     <div
-      style={{ width: '1.5rem', height: '0.2rem', backgroundColor: '#fff', borderRadius: '10px', transform: isOpen ? 'scale(0)' : 'none' }}
+      style={{
+        width: '2rem',
+        height: '0.2rem',
+        backgroundColor: '#fff',
+        transform: isOpen ? 'rotate(-45deg) translate(6px, -6px)' : 'none',
+        transition: 'transform 0.2s ease-in-out'
+      }}
     />
   </div>
 );
-
 
 export default function App() {
   const [showNavExternal, setShowNavExternal] = useState(false);
@@ -22,7 +45,7 @@ export default function App() {
   return (
     <>
       <MDBNavbar>
-        <MDBContainer fluid>
+        <MDBContainer fluid className='justify-start align-start'>
           <MDBNavbarToggler
             type='button'
             data-target='#navbarToggleExternalContent'
@@ -32,23 +55,51 @@ export default function App() {
             onClick={() => setShowNavExternal(!showNavExternal)}
             className='bg-[#2f68ad]'
           >
-          <div className='flex flex-col items-center'>
-            <MenuIcon isOpen={showNavExternal} />
-            <span className=" text-white text-[8px] font-['Open_Sans'] font-semibold tracking-[2px]" >
-            MENU
-            </span>
-          </div>
+            <div id='menu-button' className='flex flex-col items-center'>
+              <MenuIcon isOpen={showNavExternal} />
+              <span className=" text-white text-[8px] font-['Open_Sans'] font-semibold tracking-[2px] mt-1" >
+                {showNavExternal ? "CLOSE" : "MENU"}
+              </span>
+            </div>
           </MDBNavbarToggler>
-          <div className='flex'>
+          <div id='navbar-info' className="navbar-text gap-3 pr-[15px]">
           <div>
-            <p>SOUTH CAROLINA REVENUE AND FISCAL AFFAIRS OFFICE</p>
+          <a href="/">
+            <img className='w-[75px]' src={logo} alt='logo' />
+            </a>
           </div>
+          <div>
+            <div className='text-[18px] text-[#1b3a61] text-left font-bold'>
+            SOUTH CAROLINA <br /> REVENUE AND FISCAL AFFAIRS OFFICE
+            </div>
+            <div className='text-[16px] text-left'>
+            Transforming data into solutions for South Carolina
+            </div>
+          </div>
+          </div>
+          <div className='icon-container'>
+          <HiMagnifyingGlass size={24} strokeWidth={2} className="custom-icon"/>
           </div>
         </MDBContainer>
       </MDBNavbar>
 
-      <MDBCollapse show={showNavExternal}>
-        <div className='bg-light shadow-3 p-4'>
+      <MDBCollapse show={showNavExternal} className="">
+      <div className='flex w-36'>
+        <div className='left-menu bg-light shadow-3 p-4 flex flex-col '>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Home
+          </MDBBtn>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            About Us
+          </MDBBtn>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Events
+          </MDBBtn>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Boards & Committees
+          </MDBBtn>
+        </div>
+        <div className='right-menu bg-light shadow-3 p-4 flex flex-col'>
           <MDBBtn block className='border-bottom m-0' color='link'>
             Link 1
           </MDBBtn>
@@ -59,6 +110,7 @@ export default function App() {
             Link 2
           </MDBBtn>
         </div>
+      </div>
       </MDBCollapse>
     </>
   );
